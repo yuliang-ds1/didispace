30abc6c267aeff8fe057aaa9a8ef6d7b7be21972
yuliang <136214289@qq.com>
2017-04-13 10:23:31 +0800

    文件夹

--- config-client/src/main/java/com/didispace/Application.java
+++ config-client/src/main/java/com/didispace/Application.java
@@ -2,7 +2,9 @@ package com.didispace;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
--- config-server/src/main/java/com/didispace/Application.java
+++ config-server/src/main/java/com/didispace/Application.java
@@ -2,9 +2,11 @@ package com.didispace;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
